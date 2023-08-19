38f25bc1360b4602d10c98c121dc4b08836b92f0
vvs0503624459 <106775038+vvs0503624459@users.noreply.github.com>
2023-08-19 23:16:51 +0300

    start

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './container/App/App'
+import { BrowserRouter } from 'react-router-dom'
+// import { store } from "redux/store";
+// import { Provider } from "react-redux";
+// import ScrollToTop from "./ScrollToTop"
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<React.StrictMode>{/* <App /> */}</React.StrictMode>);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            {/* <Provider store={store}> */}
+            {/* <ScrollToTop/> */}
+            <App />
+            {/* </Provider> */}
+        </BrowserRouter>
+    </React.StrictMode>
+)
