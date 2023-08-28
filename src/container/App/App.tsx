71f8e4a7ca8bd138052b9442249a490fb36470f2
vvs0503624459 <106775038+vvs0503624459@users.noreply.github.com>
2023-08-28 21:40:38 +0300

    added currency changer

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,13 +1,119 @@
-import { Container } from '@mui/material'
-import Header from '../Header/Header'
+import { Button, Container, Grid, Typography } from '@mui/material'
 import './App.scss'
+import { useState } from 'react'
 type Props = {}
+type CurrencyType = {
+    [id: string]: number
+}
 
 const App = (props: Props) => {
+    const price = {
+        'iPhone 12': 750,
+        'iPhone 8': 850,
+        'iPhone X': 1250,
+    }
+    const currency = {
+        USD: 1.1,
+        EUR: 1,
+        UAH: 36,
+        PLN: 4,
+    }
+    const [currentCurrency, setCurrentCurrency] = useState<CurrencyType>({
+        EUR: 1,
+    })
+    const changecurrentCurrency = (id: string, index: number) =>
+        setCurrentCurrency(() => ({
+            [id]: index,
+        }))
+    // let currencyItem = Object.keys(currency).filter(function (string) {
+    //     return string === key
+    // })
+    let currencyItem = Object.keys(currentCurrency)
+    let currencyValue = +Object.values(currentCurrency)
+    // let currencyValue = currency.currencyItem
+
     return (
         <>
             <Container>
-                <Header />
+                <Typography variant="h4" sx={{ margin: '30px 0' }}>
+                    Our shop page
+                </Typography>
+                <div>
+                    <Button
+                        variant="outlined"
+                        onClick={() => changecurrentCurrency('USD', 1.1)}
+                    >
+                        USD
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() => changecurrentCurrency('EUR', 1)}
+                    >
+                        EUR
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() => changecurrentCurrency('UAH', 36)}
+                    >
+                        UAH
+                    </Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() => changecurrentCurrency('PLN', 4)}
+                    >
+                        PLN
+                    </Button>
+                </div>
+                <Grid className="flex">
+                    <div>
+                        <Typography
+                            variant="h4"
+                            component={'h2'}
+                            sx={{ margin: '30px 0' }}
+                        >
+                            iPhone 12
+                        </Typography>
+                        <p className="product-description">This is iPhone</p>
+                        <div className="product-price">
+                            Price: {currencyItem}
+                            <span>{price['iPhone 12'] * currencyValue}</span>
+                        </div>
+                        <Button variant="outlined">Buy</Button>
+                    </div>
+                    <div>
+                        <Typography
+                            variant="h4"
+                            component={'h2'}
+                            sx={{ margin: '30px 0' }}
+                        >
+                            iPhone 8{' '}
+                        </Typography>
+                        <p className="product-description">
+                            This is iPhone 8 ...
+                        </p>
+                        <div className="product-price">
+                            Price:{currencyItem}
+                            <span>{price['iPhone 8'] * currencyValue}</span>
+                        </div>
+                        <Button variant="outlined">Buy</Button>
+                    </div>
+                    <div>
+                        <Typography
+                            variant="h4"
+                            component={'h2'}
+                            sx={{ margin: '30px 0' }}
+                        >
+                            iPhone X
+                        </Typography>
+                        <p className="product-description">This is iPhone X</p>
+                        <div className="product-price">
+                            Price:{currencyItem}
+                            <span>{price['iPhone X'] * currencyValue}</span>
+                        </div>
+                        <Button variant="outlined">Buy</Button>
+                    </div>
+                </Grid>
+                <p className="total">total : {0}</p>
             </Container>
         </>
     )
--- src/container/Header/HeaderBody.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {}
-
-const HeaderBody = (props: Props) => {
-    return <div>HeaderBody</div>
-}
-
-export default HeaderBody
